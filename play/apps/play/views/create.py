ce75079c6b4b6f2a65d64beba62a629d5a2cf4c2
Charlie Friend <charles.d.friend@gmail.com>
2018-06-30 17:16:24 -0700

    Add error message to create view

--- play/apps/play/views/create.py
+++ play/apps/play/views/create.py
@@ -5,23 +5,39 @@ from ..apis.engine import *
 
 
 def create(request):
+    """
+    Create Game View
+
+    Creates a new game in the engine on POST.
+
+    ~~~ Returns ~~~
+    On success:
+        { "id" : (GAME ID), "href": (LINK TO GAME) }
+    On error:
+        { "error" : (ERROR MESSAGE) }
+    """
     if request.POST:
-        req = request.POST
+        try:
+            create_req = _extract_create_request(request.POST)
 
-        # TODO: handle validation errors
-        snakes = []
-        for name, url in zip(req.getlist("Snake.Name"), req.getlist("Snake.URL")):
-            if name and url:
-                snakes.append(SnakeOptions(name=name, url=url))
+            game = create_game(create_req)
+            href = "%s/games/%s" % (settings.ENGINE_URL, game.id)
 
-        create_req = CreateRequest(width=req.get("Width"),
-                                   height=req.get("Height"),
-                                   food=req.get("Food"),
-                                   snakes=snakes)
+            return JsonResponse({"id": game.id, "href": href})
 
-        game = create_game(create_req)
-        href = "%s/games/%s" % (settings.ENGINE_URL, game.id)
-
-        return JsonResponse({"id": game.id, "href": href})
+        except Exception as e:
+            return JsonResponse({"error": e.message}, status=400)
 
     return render(request, 'create.html')
+
+
+def _extract_create_request(req_data):
+    snakes = []
+    for name, url in zip(req_data.getlist("Snake.Name"), req_data.getlist("Snake.URL")):
+        if name and url:
+            snakes.append(SnakeOptions(name=name, url=url))
+
+    return CreateRequest(width=req_data.get("Width"),
+                         height=req_data.get("Height"),
+                         food=req_data.get("Food"),
+                         snakes=snakes)
