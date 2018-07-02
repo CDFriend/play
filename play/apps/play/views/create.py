f037d75f35f4176203e85ff87e79b63a3cf8ac0b
Charlie Friend <charles.d.friend@gmail.com>
2018-07-02 11:59:48 -0700

    Improve error message descriptions in create view.

--- play/apps/play/views/create.py
+++ play/apps/play/views/create.py
@@ -1,5 +1,7 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
 
 from ..apis.engine import *
 
@@ -25,19 +27,40 @@ def create(request):
 
             return JsonResponse({"id": game.id, "href": href})
 
-        except Exception as e:
-            return JsonResponse({"error": e.message}, status=400)
+        except ValidationError as e:
+            return JsonResponse({"error": e.messages}, status=400)
 
     return render(request, 'create.html')
 
 
 def _extract_create_request(req_data):
+    i = 0
     snakes = []
     for name, url in zip(req_data.getlist("Snake.Name"), req_data.getlist("Snake.URL")):
+        i += 1
         if name and url:
-            snakes.append(SnakeOptions(name=name, url=url))
+            # Name and URL specified
+            try:
+                snakes.append(SnakeOptions(name=name, url=url))
+            except ValidationError:
+                raise ValidationError(_("Invalid snake URL '%(url)s'"),
+                                      params={"url": url},
+                                      )
 
-    return CreateRequest(width=req_data.get("Width"),
-                         height=req_data.get("Height"),
-                         food=req_data.get("Food"),
-                         snakes=snakes)
+        elif (name and not url) or (url and not name):
+            # Name XOR url specified - error!
+            raise ValidationError(_("Name or URL not specified for snake %(snake_num)d"),
+                                  params={"snake_num": i})
+
+        else:
+            # Neither specified - ignore
+            pass
+
+    try:
+        req = CreateRequest(width=req_data.get("Width"),
+                            height=req_data.get("Height"),
+                            food=req_data.get("Food"),
+                            snakes=snakes)
+        return req
+    except ValueError:
+        raise ValidationError(_("Provided game options value is NaN"))
